a56e4f242a111690f5c8c59a2139c5a59ec34acd
monvex_ <toni.kononchenko@mail.ru>
2023-03-19 19:53:23 +0300

    Сделать сделал, только вот капец как похоже на чужую работу. Будьте аккуратнее

--- Элементы функционального программирования/rocket/ForcesTask.cs
+++ Элементы функционального программирования/rocket/ForcesTask.cs
@@ -1,4 +1,6 @@
-﻿namespace func_rocket;
+﻿using System;
+
+namespace func_rocket;
 
 public class ForcesTask
 {
@@ -6,15 +8,38 @@ public class ForcesTask
 	/// Создает делегат, возвращающий по ракете вектор силы тяги двигателей этой ракеты.
 	/// Сила тяги направлена вдоль ракеты и равна по модулю forceValue.
 	/// </summary>
-	public static RocketForce GetThrustForce(double forceValue) => r => Vector.Zero;
+	public static RocketForce GetThrustForce(double forceValue)
+	{
+		return r =>
+		{
+			return new Vector(Math.Cos(r.Direction) * forceValue,
+				Math.Sin(r.Direction) * forceValue); //Возвращаем вектор, который представляет собой проекцию силы тяги на ось X и Y
+		};
+	}
 
 	/// <summary>
 	/// Преобразует делегат силы гравитации, в делегат силы, действующей на ракету
 	/// </summary>
-	public static RocketForce ConvertGravityToForce(Gravity gravity, Vector spaceSize) => r => Vector.Zero;
+	public static RocketForce ConvertGravityToForce(Gravity gravity, Vector spaceSize)
+	{
+		return r => gravity(spaceSize, r.Location);
+	}
 
 	/// <summary>
 	/// Суммирует все переданные силы, действующие на ракету, и возвращает суммарную силу.
 	/// </summary>
-	public static RocketForce Sum(params RocketForce[] forces) => forces[0];
+	public static RocketForce Sum(params RocketForce[] forces) // На вход получаем массив объектов RocketForce, которые являются делегатами, возвращающими Vector
+	{
+		return r =>
+		{
+			var newX = 0; //Так как X, Y неизменяемы в классе Vector, создадим его в конце
+			var newY = 0;
+			foreach (var f in forces) 
+			{
+				newX += f(r).X; //Соотвественно, берем проекцию текущего вектора на соответствующую ось
+				newY += f(r).Y; //И прибавляем ее к итоговому вектору
+			}	
+			return new Vector(newX, newY); //Возвращаем вектор - сумму всех сил 
+		};
+	}
 }
